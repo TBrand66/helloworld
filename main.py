0f5cc8b4cd31c8647e5f635835be59a6d974c5b0
taiwo <taiwoabatan1999@gmail.com>
2023-10-30 17:13:06 +0100

    updated beginner classes

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-name = input ("what is your name? ")
-best_colour = input ('what is your best colour? ')
-print (name + ' likes ' + best_colour)
+# name = input ("what is your name? ")
+# best_colour = input ('what is your best colour? ')
+# print (name + ' likes ' + best_colour)
 
 
+# weight = input('weight: ')
+# print (0.4535 * int(weight))
 
+# weight_lbs = input('weight: ')
+# weight_kg = float(weight_lbs) * 0.45
+# print (weight_kg)
 
+# course = """
+# hi john
+# thank you for this opportunities
+# forever in your dept
+# """
+# print (course)
 
+course = 'python for beginners'
+print (course[0:])
